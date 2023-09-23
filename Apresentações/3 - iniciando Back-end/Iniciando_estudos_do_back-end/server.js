88adb999de07e3698d83e5f85cdd1d2ab7322c6b
Fernando Lucas de Castro Cavalcante <117499108+namukuzedim@users.noreply.github.com>
2023-09-23 17:06:00 -0300

    ATT-new-Stydy-Back-end
    Novos estudos do back end

---
+++ Apresentações/3 - iniciando Back-end/Iniciando_estudos_do_back-end/server.js
@@ -0,0 +1,7 @@
+const express = require ('express');
+const server = express();
+
+server.listen (5000,function() {
+    
+    console.log ("Server is runing - Hello world - this is future apache")
+} )
