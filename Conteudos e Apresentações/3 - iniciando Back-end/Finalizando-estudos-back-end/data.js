61a727402b1cadf11b31ee23442df80cca0be174
Fernando Lucas de Castro Cavalcante <117499108+massancode@users.noreply.github.com>
2023-11-18 13:42:27 -0300

    Final njk

---
+++ Conteudos e Apresentações/3 - iniciando Back-end/Finalizando-estudos-back-end/data.js
@@ -0,0 +1,25 @@
+module.exports=[
+    {
+        id:"VPjO6CfgKNk",
+        title:"QUEM É JOYBOY? TAMBEM NÃO SEI",
+        duration:"15:33 min",
+        price:"free",
+        featured: true
+
+    },
+    {
+        id:"jylg-fAVxzw",
+        title:"ELES VÃO ESTAR EM DEADPOOL 3! É A SALVAÇÃO PRO UCM E FOX?",
+        duration:"15:33 min",
+        price:"free"
+
+    },
+    {
+        id:"7u-PT6Yzq-o",
+        title:"FINALMENTE UM FILME BOM? ANÁLISE TRAILER AQUAMAN 2 E O REINO PERDIDO",
+        duration:"15:33 min",
+        price:"free",
+        featured: true
+
+    }
+]
