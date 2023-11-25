e4e78a926f536e0a144aacf497d041a1a23dbeba
Fernando Lucas de Castro Cavalcante <117499108+massancode@users.noreply.github.com>
2023-11-25 11:22:06 -0300

    Controll-de-academy
    Controll-de-academy

--- Conteudos e Apresentações/4 - Controle de academia/server.js
+++ Conteudos e Apresentações/4 - Controle de academia/server.js
@@ -1,12 +1,11 @@
   const express = require ('express');
   const server = express ();
   const nunjucks = require ('nunjucks');
-
-  const videos = require ("./data");
-
+  const routes = require ("./routes")
 
 
   server.use(express.static('public'))
+  server.use(routes)
    
   server.set ("view engine","njk")
   nunjucks.configure("views",{
@@ -15,43 +14,6 @@
     noCache:true
    
   })
-
-
-server.get("/", function(req,res){
-    const about = {
-
-       avatar_url: "https://avatars.githubusercontent.com/u/117499108?v=4",
-       name:"Fernando Lucas",
-       role:"Programador Full-Stack",
-       description:'Um dev que forma outros dev',
-       link:[
-        {name:"Github",url:"https://github.com/namukuzedim"},
-        {name:"linkedin",url:"https://www.linkedin.com/in/fernando-lucas-de-castro-309352233/"},
-        {name:"Instagram",url:"https://www.instagram.com/fernando.massan/"},
-       ]
-    }
-    return res.render("about", {about} );
-})
-server.get("/videos", function(req,res){
-    return res.render("videos",{items: videos});
-})
-  
-server.get("/video",function(req,res){
-  const id = req.query.id
-  const video = videos.find(function(video){
-
-    if (video.id==id) {
-      return true
-    }
-  
-  })
-  
-  if (!video){
-    return res.send ("Video not found")
-  }
-
-  return res.render ("video",{item: video})
-})
 
 
   server.listen(5000, function() {
